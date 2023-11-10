7bac78d6cb638135211f210f41455960d2059d25
maxkaz14 <maxkaz14@gmail.com>
2023-11-10 22:24:05 +0200

    Learning update-2 10.11.23

--- Beginning-C-Programming-From-Beginner-to-Beyond-Udemy-course/Section7/Challenge/main.cpp
+++ Beginning-C-Programming-From-Beginner-to-Beyond-Udemy-course/Section7/Challenge/main.cpp
@@ -30,12 +30,52 @@
 */
     
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
     
     // your solution goes here
+vector <int> vector1;
+vector <int> vector2;
 
-    cout << endl;
+vector1.push_back (10);
+vector1.push_back (20);
+
+cout << "Position 0 in vector1 contains: " << vector1.at(0) << endl;
+cout << "Position 1 in vector1 contains: " << vector1.at(1) << endl << endl;
+cout << "Size of vector1 is: " << vector1.size();
+
+
+vector2.push_back (100);
+vector2.push_back (200);
+
+cout << endl << endl << "Position 0 in vector2 contains: " << vector2.at(0) << endl;
+cout << "Position 1 in vector2 contains: " << vector2.at(1) << endl << endl;
+cout << "Size of vector2 is: " << vector2.size() << endl;
+cout << endl;
+
+vector <vector <int>> vector2d;
+
+vector2d.push_back (vector1);
+vector2d.push_back (vector2);
+
+cout << endl << endl << "Position 0 in vector2d contains: " << vector2d.at(0).at(0) << " " << vector2d.at(0).at(1) << endl;
+cout << "Position 1 in vector2d contains: " << vector2d.at(1).at(0) << " " << vector2d.at(1).at(1) << endl;
+cout << "Size of vector2d is: " << vector2d.size();
+cout << endl;
+
+vector1.at(0) = 1000;
+vector2d.at(0) = vector1;
+
+cout << endl << endl << "Position 0 in vector1 changed to " << vector1.at(0) << endl;
+cout << endl << endl << "Position 0 in vector2d contains: " << vector2d.at(0).at(0) << " " << vector2d.at(0).at(1) << endl;
+cout << "Position 1 in vector2d contains: " << vector2d.at(1).at(0) << " " << vector2d.at(1).at(1) << endl;
+cout << "Size of vector2d is: " << vector2d.size();
+cout << endl;
+
+    
     return 0;
 }
 
