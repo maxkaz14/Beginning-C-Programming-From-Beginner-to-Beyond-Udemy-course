3296cfa5dcdda76ffa0da2078baffcfaf415322a
maxkaz14 <maxkaz14@gmail.com>
2023-11-10 14:45:29 +0200

    Learning update 10.11.23

---
+++ Beginning-C-Programming-From-Beginner-to-Beyond-Udemy-course/ArraysPractice/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    char vowels [] {'a','o','i','e','u'};
+    
+    cout << "The first vowel is: " << vowels [0] << endl;
+    cout << "The second vowel is: " << vowels [1] << endl;
+    cout << "The last vowel is: " << vowels [4] << endl << endl;
+//    cout << "Some bizarre stuff: " << vowels [5] << endl << endl;
+    
+    return 0;
+}
